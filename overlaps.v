34bb8ad9d7a402fa2879b44f591070a4c7858224
Matt Quinn <matt@mattjquinn.com>
2019-11-23 11:53:11 -0800

    Cleanup part 2

--- overlaps.v
+++ overlaps.v
@@ -48,57 +48,47 @@ Theorem overlaps_Teradata_BigQuery_equiv : forall p1 p2 : period,
     overlapsTeradataR p1 p2 <-> overlapsBigQueryR p1 p2.
 Proof.  
   split.
-  - intros H.
-    induction H; inversion H; inversion H0; subst; apply BigQuery; repeat assumption;
-      try (apply Decidable.not_and in H2; try apply dec_ge);
-      destruct H2 as [|].
+  - intros H. inversion H; subst;
+    inversion H0; inversion H1; subst; apply BigQuery; try assumption;
+      try (apply Decidable.not_and in H3; try apply dec_ge);
+      destruct H3 as [|].
       * rewrite max_l.
-        + apply not_le in H2. apply Nat.min_glb_lt; assumption.
-        + apply not_ge in H2. rewrite Nat.lt_eq_cases. left. assumption.
-      * rewrite min_l; apply not_ge in H2.
+        + apply not_le in H3. apply Nat.min_glb_lt; assumption.
+        + apply not_ge in H3. rewrite Nat.lt_eq_cases. left. assumption.
+      * rewrite min_l; apply not_ge in H3.
         + rewrite max_l. assumption.
           rewrite Nat.lt_eq_cases. left. assumption.
         + rewrite Nat.lt_eq_cases. left. assumption.
       * rewrite max_r.
-        + apply not_ge in H2. apply Nat.min_glb_lt; assumption.
+        + apply not_ge in H3. apply Nat.min_glb_lt; assumption.
         + rewrite Nat.lt_eq_cases. left. assumption.
       * rewrite max_r.
         + rewrite min_r. assumption.
-          rewrite Nat.lt_eq_cases. apply not_ge in H2. left. assumption.
+          rewrite Nat.lt_eq_cases. apply not_ge in H3. left. assumption.
         + rewrite Nat.lt_eq_cases. left. assumption.
       * rewrite max_r.
         + rewrite min_r. assumption.
           rewrite Nat.lt_eq_cases. right. symmetry. assumption.
         + rewrite Nat.lt_eq_cases. right. assumption.
       * rewrite max_r.
-        + rewrite H1 in H3. apply Nat.min_glb_lt; assumption.
+        + rewrite H2 in H4. apply Nat.min_glb_lt; assumption.
         + rewrite Nat.lt_eq_cases. right. assumption.
-  - intros.
-    inversion H. subst.
+  - intros H. inversion H. subst.
     apply Nat.max_lub_lt_iff in H2.
     destruct H2 as [H3 H4].
     apply Nat.min_glb_lt_iff in H3.
     apply Nat.min_glb_lt_iff in H4.
     destruct H3. destruct H4.
-    inversion H. subst. clear H6. clear H7.
-    
     assert (HMQ:=Nat.max_spec (pstart p1) (pstart p2)).
-    destruct HMQ as [|].
-    destruct H6 as [].
-    
-    apply TeradataSLt.
-    apply PeriodR. assumption. assumption. assumption.
-    unfold not. intros. destruct H8 as []. intuition. intuition.
-
-    destruct H6 as [].
-    rewrite Nat.lt_eq_cases in H6. destruct H6 as [|].
-    
-    apply TeradataSGt.
-    apply PeriodR. assumption. assumption. assumption.
-    unfold not. intros. destruct H9 as []. intuition.
-
-    apply TeradataSEq.
-    apply PeriodR. assumption. assumption. auto.
-
-    decide equality.
+    destruct HMQ as [|]; destruct H6 as [].
+    * apply TeradataSLt.
+      apply PeriodR. assumption. assumption. assumption.
+      unfold not. intros. destruct H8 as []. intuition.
+    * rewrite Nat.lt_eq_cases in H6. destruct H6 as [|].
+      + apply TeradataSGt.
+        apply PeriodR. assumption. assumption. assumption.
+        unfold not. intros. destruct H8 as []. intuition.
+      + apply TeradataSEq.
+        apply PeriodR. assumption. assumption. auto.
+        decide equality.
 Qed.
